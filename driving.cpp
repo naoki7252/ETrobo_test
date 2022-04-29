fe4de2fa67fead415d44a626ff97d599460a6cef
root <root@LAPTOP-2B5BNH4N.localdomain>
2022-04-29 11:15:14 +0900

    modify driving.cpp

--- driving.cpp
+++ driving.cpp
@@ -10,7 +10,7 @@ void WheelsControl::Exec(int8_t target_power_l, int8_t target_power_r) {
 }
 
 void WheelsControl::LineTrace(Hsv curr_hsv) {
-  int8_t base_power = 50;
+  int8_t base_power = 48;
   float target_v = 40;
 
   //float mv = calcMv(curr_hsv.v);
